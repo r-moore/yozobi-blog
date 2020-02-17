d9ab51735ffa355a6c10e7f49087aff96ed9b07e
Richard Moore <richard.moore@yozobi.com>
2020-02-17 10:50:39 +0000

    Add Prism syntax highlighting for non-markdown code, default font = sans

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,41 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+import Prism from "prismjs";
 
-import { Layout } from '../components/common'
-import { MetaData } from '../components/common/meta'
+import { Layout } from "../components/common";
+import { MetaData } from "../components/common/meta";
 
 /**
-* Single post view (/:slug)
-*
-* This file renders a single post and loads all the content.
-*
-*/
+ * Single post view (/:slug)
+ *
+ * This file renders a single post and loads all the content.
+ *
+ */
 const Post = ({ data, location }) => {
-    const post = data.ghostPost
+    const post = data.ghostPost;
+
+    useEffect(() => {
+        // call the highlightAll() function to style our code blocks
+        Prism.highlightAll();
+    });
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="article"
-            />
+            <MetaData data={data} location={location} type="article" />
             <Helmet>
                 <style type="text/css">{`${post.codeinjection_styles}`}</style>
             </Helmet>
             <Layout>
                 <div className="container">
                     <article className="content">
-                        { post.feature_image ?
+                        {post.feature_image ? (
                             <figure className="post-feature-image">
-                                <img src={ post.feature_image } alt={ post.title } />
-                            </figure> : null }
+                                <img
+                                    src={post.feature_image}
+                                    alt={post.title}
+                                />
+                            </figure>
+                        ) : null}
                         <section className="post-full-content">
                             <h1 className="content-title">{post.title}</h1>
 
-                            {/* The main post content */ }
+                            {/* The main post content */}
                             <section
                                 className="content-body load-external-scripts"
                                 dangerouslySetInnerHTML={{ __html: post.html }}
@@ -45,8 +51,8 @@ const Post = ({ data, location }) => {
                 </div>
             </Layout>
         </>
-    )
-}
+    );
+};
 
 Post.propTypes = {
     data: PropTypes.shape({
@@ -54,13 +60,13 @@ Post.propTypes = {
             codeinjection_styles: PropTypes.object,
             title: PropTypes.string.isRequired,
             html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
+            feature_image: PropTypes.string
+        }).isRequired
     }).isRequired,
-    location: PropTypes.object.isRequired,
-}
+    location: PropTypes.object.isRequired
+};
 
-export default Post
+export default Post;
 
 export const postQuery = graphql`
     query($slug: String!) {
@@ -68,4 +74,4 @@ export const postQuery = graphql`
             ...GhostPostFields
         }
     }
-`
+`;
